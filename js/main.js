0472af171a91901bd0074999780cf8129e1930bd
鲁龙基 <lulj@yuntongxun.com>
2019-02-28 18:34:29 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,111 +1,190 @@
+/*********************************************************************************
 
-function main() {
+	Template Name: Karigor - Minimalist Bootstrap4 Portfolio Template
+	Description: A perfect minimal template to build beautiful and unique portfolio websites. It comes with nice and clean design.
+	Version: 1.0
 
-(function () {
-   'use strict';
-   
-   // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+	Note: This is main js.
 
-   // Testimonial Slider
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
+**********************************************************************************/
 
-    /*====================================
-    Show Menu on Book
-    ======================================*/
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 100;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
-
-    $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 80
-    })
-
-  	$(document).ready(function() {
-  	    $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
-
-  	});
-
-  	/*====================================
-    Portfolio Isotope Filter
-    ======================================*/
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
-    });
+/**************************************************************
 	
-	// ****************************************************************
-// counterUp
-// ****************************************************************
+	STYLESHEET INDEXING
+	|
+	|
+	|___ Sticky Header
+	|___ Testimonial Slider Active
+	|___ Bootstrap4 Tooltip Active
+	|___ Portfolio Filter & Popup Active
+	|___ Header Menu Effect
+	|___ Mobile Menu
+	|___ Boxed Layout
+	|___ Counter Active
+	|___ Radial Progress
+	|___ Blog Item Height
+	|___ Sticky Sidebar
+	|
+	|
+	|___ END STYLESHEET INDEXING
 
-	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){	
-			$('span.count').counterUp({
-					delay: 10, // the delay time in ms
-			time: 1000 // the speed time in ms
-			});
+***************************************************************/
+
+
+(function ($) {
+	'use strict';
+
+
+
+	/* Sticky Header */
+	$(window).on('scroll', function () {
+		var scrollPos = $(this).scrollTop();
+		if (scrollPos > 200) {
+			$('.sticky-header').addClass('is-sticky');
+		} else {
+			$('.sticky-header').removeClass('is-sticky');
 		}
 	});
 
-  	/*====================================
-    Pretty Photo
-    ======================================*/
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
-
-}());
 
 
-}
-main();
+	/* Testimonial Slider Active */
+	$('.testimonial-slider').slick({
+		slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		dots: true,
+		arrows: false,
+		easing: 'ease-in-out',
+	});
+
+
+
+
+
+	/* Bootstrap4 Tooltip Active */
+	$('[data-toggle="tooltip"]').tooltip();
+
+
+
+
+
+	/* Portfolio Filter & Popup Active */
+	function portfolioFilterLightgallery(){
+		var $gallery = $('.portfolios-wrapper');
+		var $boxes = $('.portfolio-item');
+		$boxes.hide();
+
+		$gallery.imagesLoaded({
+			background: true
+		}, function () {
+			$boxes.fadeIn();
+			$gallery.isotope({
+				itemSelector: '.portfolio-item',
+				layoutMode: 'masonry',
+				masonry: {
+					columnWidth: 1,
+				}
+			});
+		});
+
+		$('.portfolio-filters button').on('click', function () {
+			
+			var filterValue = $(this).attr('data-filter');
+			$gallery.isotope({
+				filter: filterValue
+			});
+			$gallery.data('lightGallery').destroy(true);
+			$('.portfolios-wrapper').lightGallery({
+				selector: filterValue.replace('*', '') + ' .portfolio-zoom-button',
+			});
+
+			$('.portfolio-filters button').removeClass('is-checked');
+			$(this).addClass('is-checked');
+
+		});
+
+		$('.portfolios-wrapper').lightGallery({
+			selector: '.portfolio-item .portfolio-zoom-button'
+		});
+	}
+	portfolioFilterLightgallery();
+
+
+
+
+	/* Header Menu Effect */
+	$('.header-navigation-trigger').on('click', function(){
+		$(this).toggleClass('is-active');
+		$('.main-navigation').toggleClass('is-visible');
+	});
+
+
+
+
+	/* Mobile Menu */
+	$('nav.main-navigation').meanmenu({
+		meanMenuClose: '<img class="black" src="img/icons/icon-close.png" alt="close icon"><img class="white" src="img/icons/icon-close-white.png" alt="close icon">',
+		meanMenuCloseSize: '18px',
+		meanScreenWidth: '991',
+		meanExpandableChildren: true,
+		meanMenuContainer: '.mobile-menu',
+		onePage: true
+	});
+	
+
+
+
+	/* Boxed Layout */
+	if( $('.boxed-layout').length ){
+		$('body').css('background-color', '#f1f1f1');
+	}
+
+
+
+
+	/* Counter Active */
+	$('.counter-active').counterUp({
+		delay: 10,
+		time: 1000
+	});
+
+
+
+
+
+	/* Radial Progress */
+	$('.radial-progress').waypoint(function(){
+
+		$('.radial-progress').easyPieChart({
+			lineWidth: 3,
+			trackColor: false,
+			scaleLength: 0,
+			rotate: -45,
+			barColor: '#555555'
+		});
+
+	}, {
+		triggerOnce: true,
+		offset: 'bottom-in-view'
+	});
+
+
+
+
+	/* Blog Item Height */
+	$('.blog-item').matchHeight();
+
+
+
+
+	/* Sticky Sidebar */
+	$('.sticky-sidebar-active').theiaStickySidebar({
+		additionalMarginTop: 30,
+		additionalMarginBottom: 30
+	});
+
+
+
+
+})(jQuery);
